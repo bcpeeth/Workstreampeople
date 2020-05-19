efe5719f384c3767024cefb0dd7e44f1da44b687
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2020-05-19 23:00:02 +0200

    'Destructuring' full example

--- Javascript/Destructuring.js
+++ Javascript/Destructuring.js
@@ -8,7 +8,7 @@ var { species, sound } = animal
 console.log('The ' + species + ' says ' + sound + '!')
 
 //  Destructuring is used to make it easier to deal with option objects.
-//Option objects are objects that you pass to a function which is a single object that might or might not contain several properties.
+// Option objects are objects that you pass to a function which is a single object that might or might not contain several properties.
 
 makeSound ({
     species: 'dog',
@@ -18,4 +18,43 @@ makeSound ({
 
 function makeSound(options) {
     console.log('The ' + options.species + ' says ' + options.sound + '!')
+}
+
+// It turns a more complicated when we make 'species' an optional property on the object's object.
+
+
+makeSound ({
+    weight: 23,
+    sound: 'woof',
+})
+
+function makeSound(options) {
+    options.species = options.species || 'animal'
+    console.log('The ' + options.species + ' says ' + options.sound + '!')
+}
+
+// The method above becomes cluttered, with code being repeated too much. This can be solved without destructuring by declaring variables at the top of the function.
+
+makeSound ({
+    weight: 23,
+    sound: 'woof',
+})
+
+function makeSound(options) {
+    // var species = options.species || 'animal'
+    // var sound = options.sound
+    var { species, sound } = options
+    species = species || 'animal'
+    console.log('The ' + species + ' says ' + sound + '!')
+}
+
+// To optimize even more, we can do destructuring in te method signature and assign default values in it.
+
+makeSound ({
+    weight: 23,
+    sound: 'woof',
+})
+
+function makeSound({ species = 'animal', sound }) {
+    console.log('The ' + species + ' says ' + sound + '!')
 }
