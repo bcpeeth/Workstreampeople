aded8dcac5ddbc97453a84221bc627d9ff65afcb
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2020-06-07 17:49:12 +0200

    Finishes 'map' episode

--- Javascript/Map.js
+++ Javascript/Map.js
@@ -16,10 +16,27 @@ var animals = [
 //   names.push(animals[i].name)
 // }
 
+// 'Filter' expects its callback function to return a true or false value that determines wether or not the item should be included in the array.
+// 'Map' will include all items in the array, but instead, it expects the callback function to return a transformed object that it will put into the new array, instead of the original 'animal', whcih in this case will be the name.
 
-// Now using 'map':
+// var names = animals.map(function(animal) {
+//    return animal.name
+// })
 
-var names = animals.map(function(animal) {
-   
-})
+// Since 'map' just expects the callback to return any object, we can use it to create completely new objects.
+
+// var names = animals.map(function(animal) {
+//   return animal.name + 'is a ' + animal.species
+// })
+
+// We can further optimize the code above by using 'arrow functions'.
+
+// var names = animals.map((animal) => {
+//   return animal.name + 'is a ' + animal.species
+// })
+
+// var names = animals.map((animal) => animal.name)
+
+var names = animals.map((x) => x.name)
+
 console.log(names)
